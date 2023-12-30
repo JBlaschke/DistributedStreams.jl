cbc4e9c01cd1dfb14a490f29e9607fd18c2c6498
Johannes Blaschke <johannes@blaschke.science>
2023-12-29 21:50:52 -0800

    update the serialization code, and wip on named tuples

--- src/bits.jl
+++ src/bits.jl
@@ -2,12 +2,24 @@ module Bits
 
 using StructIO
 
+abstract type SerializedNamedTuple end
+
 struct NodeHeader
     size::UInt64
     type::UInt8
+
+    function NodeHeader(type::DataType, data_length)
+        type_idx = findfirst(x->x[1]==type, TYPES)
+
+        if isabstracttype(type)
+            return new(data_length, type_idx)
+        end
+
+        new(packed_sizeof(type)*data_length, type_idx)
+    end
 end
 
-TYPES = Tuple{DataType, Bool}[
+const TYPES = Tuple{DataType, Bool}[
     (Bool, false),
     (UInt8, false),
     (UInt16, false),
@@ -45,22 +57,20 @@ TYPES = Tuple{DataType, Bool}[
     (Rational{Int32}, false),
     (Rational{Int64}, false),
     (Rational{Int128}, false),
+    (SerializedNamedTuple, true),
     (String, true)
 ]
 
+HEADER_SIZE = packed_sizeof(NodeHeader)
+
 custom_convert_to(data::String) = Vector{UInt8}(data)
-custom_convert_from(::Type{String}, data::AbstractArray{UInt8}) = String(data)
 
 # Default to byte-size when checking the size of String
 StructIO.packed_sizeof(::Type{String}) = packed_sizeof(UInt8)
 
 function to_bits(data::AbstractArray, type)
-    data_len = length(data)
-    type_idx = findfirst(x->x[1]==type, TYPES)
-    type_len = packed_sizeof(type)
-
     # Header
-    header = NodeHeader(type_len*data_len, type_idx)
+    header = NodeHeader(type, length(data))
     header_size = packed_sizeof(NodeHeader)
 
     # Create bits buffer
@@ -82,16 +92,81 @@ to_bits(data::AbstractString) = to_bits(custom_convert_to(data), String)
 # TODO: flag quantities as "scalar"
 to_bits(data::Number) = to_bits([data])
 
+function to_bits(data::T) where T <: NamedTuple
+    # Preamble tags that N key(string), value pairs are to follow
+    preamble = NodeHeader(SerializedNamedTuple, length(keys(data)))
+    preamble_size = packed_sizeof(NodeHeader)
+
+    buffer = Vector{UInt8}(undef, preamble_size)
+    buffer .= 0x00
+
+    writebuf = IOBuffer(buffer; read=false, write=true)
+    pack(writebuf, preamble)
+
+    for k in keys(data)
+        keybuff = to_bits(String(k))
+        valbuff = to_bits(data[k])
+        write(writebuf, keybuff)
+        write(writebuf, valbuff)
+    end
+
+    return buffer
+end
+
 export to_bits
 
-function from_bits(bits::Vector{UInt8})
-    header_bits = @view bits[1:packed_sizeof(NodeHeader)]
+custom_convert_from(::Type{String}, data::AbstractArray{UInt8}) = String(data)
+
+function custom_convert_from(
+        ::Type{SerializedNamedTuple}, data::AbstractArray{UInt8}
+    )
+
+    # preamble_bits = @view bits[1:HEADER_SIZE]
+    # preamble_buf = IOBuffer(preamble_bits; read=true, write=false)
+    # preamble = unpack(header_buf, NodeHeader)
+
+    data_dict = Dict{Symbol, Any}()
+    # idx += HEADER_SIZE
+    idx = 1
+
+    while true # preamble.size
+        header_bits = @view bits[idx:idx + HEADER_SIZE]
+        header_buf = IOBuffer(header_bits; read=true, write=false)
+        header = unpack(header_buf, NodeHeader)
+
+        type, _ = TYPES[header.type]
+        @assert type == String
+
+        key = custom_convert_from(String, @view bits[
+            idx + HEADER_SIZE:idx + HEADER_SIZE + header.size
+        ])
+
+        val = from_bits(bits; complete=false)
+        idx += HEADER_SIZE + header.size
+        if idx >= length(data)
+            break
+        end
+    end
+end
+
+function from_bits(bits::Vector{UInt8}; complete=true, idx=Ref(0))
+    header_bits = @view bits[1:1 + HEADER_SIZE]
     header_buf = IOBuffer(header_bits; read=true, write=false)
     header = unpack(header_buf, NodeHeader)
     type, use_constructor = TYPES[header.type]
 
-    data = @view bits[packed_sizeof(NodeHeader)+1:end] 
-    @assert length(data) == header.size
+    # Some optimization around @view gives me a 25% perf boost if I use `end` in
+    # a `@view` => the `complete` flag is used to indicate that the `bits`
+    # array is a complete record (as opposed to a larger record or which we're)
+    # processing only a chunk
+    if complete || isabstracttype(type)
+        data = @view bits[1 + HEADER_SIZE:end]
+    else
+        data = @view bits[1 + HEADER_SIZE:HEADER_SIZE + header.size]
+    end
+
+    idx[] += HEADER_SIZE + header.size
+
     if ! use_constructor
         return reinterpret(type, data)
     else
--- test/test_serialization.jl
+++ test/test_serialization.jl
@@ -6,23 +6,43 @@ using Test, DistributedStreams
 #-------------------------------------------------------------------------------
 
 val = 10
+idx = Ref(0)
 x = DistributedStreams.Bits.to_bits(val)
-@test DistributedStreams.Bits.from_bits(x)[1] == val
+@test DistributedStreams.Bits.from_bits(x; idx=idx)[1] == val
+@test idx[] == length(x)
 
 val = 10.1
+idx = Ref(0)
 x = DistributedStreams.Bits.to_bits(val)
-@test DistributedStreams.Bits.from_bits(x)[1] == val
+@test DistributedStreams.Bits.from_bits(x; idx=idx)[1] == val
+@test idx[] == length(x)
 
 val = Complex(10, 2)
+idx = Ref(0)
 x = DistributedStreams.Bits.to_bits(val)
-@test DistributedStreams.Bits.from_bits(x)[1] == val
+@test DistributedStreams.Bits.from_bits(x; idx=idx)[1] == val
+@test idx[] == length(x)
 
 val = Rational(1, 2)
+idx = Ref(0)
 x = DistributedStreams.Bits.to_bits(val)
-@test DistributedStreams.Bits.from_bits(x)[1] == val
+@test DistributedStreams.Bits.from_bits(x; idx=idx)[1] == val
+@test idx[] == length(x)
 
 val = "hello there"
+idx = Ref(0)
 x = DistributedStreams.Bits.to_bits(val)
-@test DistributedStreams.Bits.from_bits(x) == val
+@test DistributedStreams.Bits.from_bits(x; idx=idx) == val
+@test idx[] == length(x)
 
+# Test very large random inputs -- to check eg. for off-by-one errors in at the
+# end of the NodeHeader struct
+
+using Random
+
+val = randstring(1_000_000_000)
+idx = Ref(0)
+x = DistributedStreams.Bits.to_bits(val)
+@test DistributedStreams.Bits.from_bits(x; idx=idx) == val
+@test idx[] == length(x)
 #-------------------------------------------------------------------------------
